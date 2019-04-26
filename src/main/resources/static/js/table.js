a7612d55737ac0cf5250315d3c98806270d7827c
nataliexmg <nataliemegumigilbert@gmail.com>
2019-04-26 16:59:59 +0000

    working mock response for table

--- src/main/resources/static/js/table.js
+++ src/main/resources/static/js/table.js
@@ -1,5 +1,41 @@
-var myObj, i, txt = "";
-                    myObj = [
+var payload, myObj, myObj2, i, txt = "";
+
+           payload = [
+           {
+               "name": "Aaron Pavlick",
+               "pointsBalance": 600,
+               "tier": 0,
+               "pointToNextLevel": 1400,
+               "table": [
+                   {
+                       "product": "PNC ONYX Card",
+                       "transactionCatagory": "TRAVEL",
+                       "vendor": null,
+                       "transactionAmount": 50,
+                       "pointAmount": 150
+                   },
+                   {
+                       "product": "PNC ONYX Card",
+                       "transactionCatagory": "TRAVEL",
+                       "vendor": null,
+                       "transactionAmount": 50,
+                       "pointAmount": 150
+                   },
+                   {
+                       "product": "PNC ONYX Card",
+                       "transactionCatagory": "TRAVEL",
+                       "vendor": null,
+                       "transactionAmount": 50,
+                       "pointAmount": 150
+                   }
+               ]
+           }];
+
+         myObj = payload[0].table;
+         console.log("payload[4].table = "+myObj);
+
+
+                    myObj2 = [
                          {
                          "name":"John",
                          "age":"30",
@@ -20,13 +56,13 @@ var myObj, i, txt = "";
                          ];
 
 
-                    txt += "<table class=\"table\"><thead><tr><th>Column1</th><th>Column2</th><th>Column3</th><th>Column4</th><th>Column5</th></tr></thead><tbody>";
+                    txt += "<table class=\"table\"><thead><tr><th>Product</th><th>Transaction Category</th><th>Vendor</th><th>Transaction Amount</th><th>Point Amount</th></tr></thead><tbody>";
                     for (i = 0; i < myObj.length; i++) {
-                        txt += "<tr><td class=>" + myObj[i].name + "</td>";
-                        txt += "<td>" + myObj[i].age + "</td>";
-                        txt += "<td>" + myObj[i].cars + "</td>";
-                        txt += "<td>" + myObj[i].animal + "</td>";
-                        txt += "<td>" + myObj[i].color + "</td></tr>";
+                        txt += "<tr><td class=>" + payload[0].table[i].product + "</td>";
+                        txt += "<td>" + payload[0].table[i].transactionCatagory + "</td>";
+                        txt += "<td>" + payload[0].table[i].vendor + "</td>";
+                        txt += "<td>" + payload[0].table[i].transactionAmount + "</td>";
+                        txt += "<td>" + payload[0].table[i].pointAmount + "</td></tr>";
                         }
                         txt += "</tbody></table>";
                     document.getElementById("demo").innerHTML = txt;
