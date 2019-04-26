10631f28da40eeaa043649b552a949d64845b014
nataliexmg <nataliemegumigilbert@gmail.com>
2019-04-26 13:17:41 +0000

    working table in html

--- src/main/resources/static/js/updateUserInfo.js
+++ src/main/resources/static/js/updateUserInfo.js
@@ -8,21 +8,10 @@ function sendRequest(){
 }
 
 function displayResponse() {
-    var res_block = document.getElementById("response-block");
-    var blue_bar = document.getElementById("blue-bar");
     var trans_table = document.getElementById("transTable");
-    res_block.style.display='block';
-    blue_bar.style.display='block';
     trans_table.style.display='block';
 
-    data = {"vendor":"starbucks","transactioncategory":"food","amount":"$4.00","postedDate":"9/22/2014","ptsEarned":"12"};
-    var tr;
-    for (var i = 0; i < data.length; i++) {
-        tr = $('<tr/>');
-        tr.append("<td>" + data[i].vendor + "</td>");
-        tr.append("<td>" + data[i].transactioncategory + "</td>");
-        tr.append("<td>" + data[i].amount + "</td>");
-        $('table').append(tr);
-    }
+    var data_table = document.getElementById("dataTable");
+    data_table.style.display='block';
 
 }
